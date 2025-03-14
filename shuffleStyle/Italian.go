a73b719b1081725823edcec348555182d2358135
BecioProton <vincenzo.iovine@protonmail.com>
2025-03-14 09:29:23 +0000

    update italianStyle Shuffle

--- shuffleStyle/Italian.go
+++ shuffleStyle/Italian.go
@@ -8,27 +8,33 @@ import (
 
 // ItalianShuffle simulates an Italian Shuffle on the Deck struct
 func ItalianShuffle(d *cards.Deck) {
-	// Split deck into two halves
-	mid := len(d.Cards) / 2
-	half1 := d.Cards[:mid]
-	half2 := d.Cards[mid:]
-
-	// Create a new shuffled deck
-	shuffledDeck := []cards.Card{}
 	rand.Seed(time.Now().UnixNano())
 
-	// Interleave the two halves with randomness
-	for len(half1) > 0 || len(half2) > 0 {
-		// Randomly decide whether to pick from half1 or half2
-		if len(half1) > 0 && rand.Float64() > 0.5 {
-			shuffledDeck = append(shuffledDeck, half1[0])
-			half1 = half1[1:]
-		} else if len(half2) > 0 {
-			shuffledDeck = append(shuffledDeck, half2[0])
-			half2 = half2[1:]
-		}
-	}
+	// Randomize the number of times to repeat the shuffle (e.g., 3 to 7 times)
+	times := rand.Intn(5) + 5 // Generates a number between 3 and 7
 
-	// Update the original deck with the shuffled deck
-	d.Cards = shuffledDeck
+	for i := 0; i < times; i++ {
+		// Split the deck into two halves
+		mid := len(d.Cards) / 2
+		half1 := d.Cards[:mid]
+		half2 := d.Cards[mid:]
+
+		// Create a new shuffled deck
+		shuffledDeck := []cards.Card{}
+
+		// Interleave the two halves with randomness
+		for len(half1) > 0 || len(half2) > 0 {
+			if len(half1) > 0 && rand.Float64() > 0.5 {
+				shuffledDeck = append(shuffledDeck, half1[0])
+				half1 = half1[1:]
+			} else if len(half2) > 0 {
+				shuffledDeck = append(shuffledDeck, half2[0])
+				half2 = half2[1:]
+			}
+		}
+
+		// Update the original deck with the shuffled deck
+		d.Cards = shuffledDeck
+	}
+	println("Shuffled", times, "times")
 }
